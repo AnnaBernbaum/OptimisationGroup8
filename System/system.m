7297a46c98c1dec550ef243d31770ceeb5eecb09
Anna Bernbaum <anna@bernbaum.co.uk>
2018-12-12 21:17:53 +0000

    system Pseudoscript

--- System/system.m
+++ System/system.m
@@ -1,1 +1,18 @@
-%% This is a MATLAB script ... 
+
+
+% Find cost combinations of plate
+
+% Find cost of combinations drum
+
+% Maximum spend is £1 
+
+% Generate all possible combinations that fall under this price
+
+% Find heat flux for each combination
+
+% Find drum speed for each combination
+
+% Normalise the 2 data sets
+
+% Weight between the two to give a value
+% Choose the minium value
